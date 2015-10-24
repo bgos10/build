0c4534cd39685e1e28572628096c594b3987cbf5
LorDClockaN <davor@losinj.com>
2015-10-24 16:30:23 +0200

    String cleanup and remove Unmount system after SuperSu flash
    Change-Id: I35519779f26f58237bbc7f3a21a014c15d81b6bd
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    
    Conflicts:
    	tools/releasetools/common.py

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1299,14 +1299,16 @@ class BlockDifference(object):
       script.AppendExtra('if range_sha1("%s", "%s") == "%s" then' % (
                          self.device, ranges_str,
                          self._HashZeroBlocks(self.tgt.extended.size())))
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("OrionOs System files verified.")
       script.AppendExtra(
           'else\n'
           '  abort("%s partition has unexpected non-zero contents after OTA '
           'update");\n'
           'endif;' % (partition,))
     else:
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("OrionOs System files verified.")
 
     script.AppendExtra(
         'else\n'
