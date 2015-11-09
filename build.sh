69b0ba91a85871e615c2cbc118e765769de78a01
Akhil Narang <akhilnarang.1999@gmail.com>
2015-11-09 23:12:37 +0530

    build: add a simple build script
    *usage: bash build.sh bacon
    *to build for bacon
    *only devices in vendor/orion/orion.devices will be official, rest unofficial
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+device=$1;
+if [ "$device" == "" ];
+then
+echo "Please specify the device as a parameter";
+exit 1;
+fi
+if [ ! -f "prebuilts/chromium/$device/media/bootanimation.zip" ]; then
+mkdir -p ~/orion/prebuilts/chromium/$device/media
+cp vendor/orion/config/media/bootanimation.zip prebuilts/chromium/$device/media/bootanimation.zip
+fi
+if grep -Fxq $device vendor/orion/orion.devices
+then
+export ORION_RELEASE=true
+else
+unset ORION_RELEASE
+fi
+lunch orion_$device-userdebug
+mka orion
