LineageOS/android_build

product_config: Set CM_BUILD priority above TARGET_BUILD_APPS
Even if we are doing TARGET_BUILD_APPS, we may want to use a cm
target for building.

Change-Id: Ic8053bc679fa8b726be90e611aeaacdf1850cabc

core/product_config.mk
@@ -179,19 +179,20 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
-    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+# A CM build needs only the CM product makefiles.
+ifneq ($(CM_BUILD),)
+  all_product_configs := $(shell ls device/*/$(CM_BUILD)/cm.mk)
 else
-  ifneq ($(CM_BUILD),)
-    all_product_configs := $(shell ls device/*/$(CM_BUILD)/cm.mk)
+  ifneq ($(strip $(TARGET_BUILD_APPS)),)
+  # An unbundled app build needs only the core product makefiles.
+  all_product_configs := $(call get-product-makefiles,\
+      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # CM_BUILD
-endif
+  endif # TARGET_BUILD_APPS
+endif # CM_BUILD
 
 ifeq ($(CM_BUILD),)
 # Find the product config makefile for the current product.

bgos10/build

core/product_config.mk
@@ -179,19 +179,20 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-ifneq ($(strip $(TARGET_BUILD_APPS)),)
-# An unbundled app build needs only the core product makefiles.
-all_product_configs := $(call get-product-makefiles,\
-    $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
+# A CM build needs only the CM product makefiles.
+ifneq ($(SLIM_BUILD),)
+  all_product_configs := $(shell ls device/*/$(SLIM_BUILD)/cm.mk)
 else
-  ifneq ($(SLIM_BUILD),)
-    all_product_configs := $(shell ls device/*/$(SLIM_BUILD)/slim.mk)
+  ifneq ($(strip $(TARGET_BUILD_APPS)),)
+  # An unbundled app build needs only the core product makefiles.
+  all_product_configs := $(call get-product-makefiles,\
+      $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif
-endif
+  endif # TARGET_BUILD_APPS
+endif # SLIM_BUILD
 
 ifeq ($(SLIM_BUILD),)
 # Find the product config makefile for the current product.