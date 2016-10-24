6f346145d97cfeb44d7c9592264b7fc6a68588d4
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-10-24 05:16:10 +0200

    Make clang configurable via boardconfig. Make uberclang default
    Signed-off-by: bgos10 <bishalgoswami@gmail.com>

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -2,6 +2,6 @@
 
 SDCLANG_LTO_DEFS := build/core/sdllvm-lto-defs.mk
 
-LLVM_RELEASE_VERSION := 3.8
-LLVM_PREBUILTS_VERSION := 3.8
+LLVM_RELEASE_VERSION ?= uber-3.8
+LLVM_PREBUILTS_VERSION ?= uber-3.8
 LLVM_PREBUILTS_BASE ?= prebuilts/clang/host
