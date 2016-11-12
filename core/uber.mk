1e363342a1e015cc67e42b6816f4a0fbb5fa2886
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-11-12 02:09:46 +0100

    Use sdclang instead of custom clang Also use newer clang from google for RS Apply polly optimizations to more process and to sdclang Revisits how O3 is applied Remove the need for different RS clang.
    Signed-off-by: bgos10 <bishalgoswami@gmail.com>

--- core/uber.mk
+++ core/uber.mk
@@ -33,7 +33,9 @@ STRICT_CLANG_LEVEL := \
 ############
 # GRAPHITE #
 ############
-LOCAL_DISABLE_GRAPHITE :=
+LOCAL_DISABLE_GRAPHITE := \
+	libfec_rs \
+        libfec_rs_host \
 
 GRAPHITE_FLAGS := \
 	-fgraphite \
@@ -50,18 +52,12 @@ GRAPHITE_FLAGS := \
 #########
 
 # Polly flags for use with Clang
-POLLY :=-mllvm -polly \
-	-mllvm -polly-parallel \
-	-mllvm -polly-ast-use-context \
-	-mllvm -polly-vectorizer=polly \
-	-mllvm -polly-opt-fusion=max \
-	-mllvm -polly-opt-maximize-bands=yes \
-	-mllvm -polly-run-dce \
-	-mllvm -polly-dependences-computeout=0 \
-	-mllvm -polly-dependences-analysis-type=value-based \
-	-mllvm -polly-run-inliner \
-	-mllvm -polly-detect-keep-going \
-	-mllvm -polly-rtc-max-arrays-per-group=40
+POLLY := -mllvm -polly \
+	 -mllvm -polly-parallel -lgomp \
+	 -mllvm -polly-vectorizer=polly \
+	 -mllvm -polly-opt-fusion=max \
+	 -mllvm -polly-opt-maximize-bands=yes \
+	 -mllvm -polly-run-inliner
 
 # Those are mostly Bluetooth modules
 DISABLE_POLLY_O3 := \
@@ -72,8 +68,7 @@ DISABLE_POLLY_O3 := \
 	bluetooth.mapsapi \
         bluetooth.default \
         bluetooth.mapsapi \
-	libart \
-        libart-compiler \
+	libart% \
         libbluetooth_jni \
         libbt% \
         libosi \
@@ -87,43 +82,20 @@ DISABLE_POLLY_O3 := \
 
 # Disable modules that dont work with Polly. Split up by arch.
 DISABLE_POLLY_arm := \
-	healthd \
-	libaudioflinger \
-	libavcdec \
-        libavcenc \
-	libbnnmlowp \
-	libcrypto \
-	libcrypto_static \
-	libF77blas \
-	libFFTEm \
+	libandroid \
 	libFraunhoferAAC \
-	libicuuc \
-	libinputflinger \
-	libjni_filtershow_filters \
-	libjni_snapcammosaic \
 	libjpeg_static \
-	libLLVM% \
-	libmpeg2dec \
-	libmusicbundle \
 	libopus \
 	libpdfium% \
-	libreverb \
-	libRS_internal \
-	libsonic \
 	libskia_static \
-	libstagefright% \
-	libv8 \
-	libvpx \
-	libwebp-decode \
-        libwebp-encode \
-	libwebrtc% \
-	recovery
+	libstagefright%
 
 DISABLE_POLLY_arm64 := \
 	$(DISABLE_POLLY_arm) \
 	libaudioutils \
 	libmedia_jni \
 	libRSCpuRef \
+	libavutil \
 	libscrypt_static \
 	libsvoxpico
 
@@ -133,19 +105,42 @@ LOCAL_DISABLE_POLLY := \
   $(DISABLE_POLLY_O3)
 
 # Set POLLY based on DISABLE_POLLY
+ifeq ($(LOCAL_CLANG),false)
+  POLLY :=
+endif
+
 ifeq (1,$(words $(filter $(LOCAL_DISABLE_POLLY),$(LOCAL_MODULE))))
   POLLY :=
 endif
 
+
+my_cflags := $(filter-out -Wall -Werror -g -O3 -O2 -Os -O1 -O0 -Og -Oz -Wextra -Weverything,$(my_cflags))
+
+ifeq ($(O3_OPTS),true)
+  ifeq (1,$(words $(filter $(DISABLE_POLLY_O3),$(LOCAL_MODULE))))
+      my_cflags += -O2
+  else
+      my_cflags += -O3
+  endif
+else
+  my_cflags += -O2
+endif
+
 ifeq ($(my_sdclang), true)
+  ifndef LOCAL_IS_HOST_MODULE
+    # Enable Polly if not blacklisted.
+    # Don't show unused warning on Clang and GCC
+    my_cflags += $(POLLY)
+  endif
+
   ifeq ($(my_clang),true)
     my_cflags += -Qunused-arguments
   else
     my_cflags += -Wno-unknown-warning
   endif
+
 else ifeq ($(my_clang),true)
   ifndef LOCAL_IS_HOST_MODULE
-    my_cflags := $(filter-out -g,$(my_cflags))
     # Enable Polly if not blacklisted.
     # Don't show unused warning on Clang and GCC
     my_cflags += $(POLLY) -Qunused-arguments
@@ -177,14 +172,3 @@ ifeq ($(GRAPHITE_OPTS),true)
   endif
 endif
 
-ifeq ($(O3_OPTS),true)
-  my_cflags := $(filter-out -Wall -Werror -g -O3 -O2 -Os -O1 -O0 -Og -Oz -Wextra -Weverything,$(my_cflags))
-  ifeq (1,$(words $(filter $(DISABLE_POLLY_O3),$(LOCAL_MODULE))))
-      my_cflags += -O2
-  else
-      my_cflags += -O3
-  endif
-else
-  my_cflags := $(filter-out -Wall -Werror -g -O3 -O2 -Os -O1 -O0 -Og -Oz -Wextra -Weverything,$(my_cflags))
-  my_cflags += -O2
-endif
